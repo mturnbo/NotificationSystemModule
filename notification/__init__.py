cca0335877d28bd2506cf02e064105b81ca3d5c2
Marcus Turnbo <marcus@emarcus.com>
2026-06-15 16:24:48 -0700

    add NotificationFactory and update package exports

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,1 +1,3 @@
 from .base import NotificationProvider
+from .service import NotificationService
+from .factory import NotificationFactory
---
+++ notification/factory.py
@@ -0,0 +1,16 @@
+from .providers import EmailProvider, SMSProvider, SlackProvider, DiscordProvider
+
+
+class NotificationFactory:
+    @staticmethod
+    def create(provider_type: str):
+        providers = {
+            "email": EmailProvider,
+            "sms": SMSProvider,
+            "slack": SlackProvider,
+            "discord": DiscordProvider,
+        }
+        provider_class = providers.get(provider_type.lower())
+        if not provider_class:
+            raise ValueError(f"Unknown provider: {provider_type}")
+        return provider_class()
