84c1da3564e1f5171f1978e19d82a849259aba48
Marcus Turnbo <marcus@emarcus.com>
2026-06-15 17:55:20 -0700

    implement EmailProvider with SMTP and env var config

--- notification/providers/email.py
+++ notification/providers/email.py
@@ -1,10 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
 from notification.base import NotificationProvider
 
 
 class EmailProvider(NotificationProvider):
     def __init__(self, recipients: list[str]):
         self.recipients = recipients
+        self.host = os.environ["SMTP_HOST"]
+        self.port = int(os.environ["SMTP_PORT"])
+        self.user = os.environ["SMTP_USER"]
+        self.password = os.environ["SMTP_PASSWORD"]
+        self.from_addr = os.environ["SMTP_FROM"]
 
     def send(self, message: str) -> None:
-        # TODO: replace print with real SMTP call
-        print(f"EMAIL to {self.recipients}: {message}")
+        msg = MIMEText(message)
+        msg["Subject"] = message.splitlines()[0]
+        msg["From"] = self.from_addr
+        msg["To"] = ", ".join(self.recipients)
+
+        with smtplib.SMTP(self.host, self.port) as smtp:
+            smtp.starttls()
+            smtp.login(self.user, self.password)
+            smtp.sendmail(self.from_addr, self.recipients, msg.as_string())
